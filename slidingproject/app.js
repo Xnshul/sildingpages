af2107114596376cb42e38cb2c99a0b1d9e27ffc
Anshul koshta <ANSHULKOSHTA621@GMAIL.COM>
2025-01-25 00:07:09 +0530

    Delete slidingproject directory
    added fixed files

--- slidingproject/app.js
+++
@@ -1,7 +0,0 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
-
-next.addEventListener('click', function(){let items = document.querySelectorAll('.slide').appendChild(items[0])})
-
-prev.addEventListener('click', function(){let items = document.querySelectorAll('.slide').prepend(items.length-1)})
-
